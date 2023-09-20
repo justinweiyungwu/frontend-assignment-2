9f1820f42508c03becc4544a2fb7db6f743ab591
CodeSandbox <support@codesandbox.io>
2023-09-20 20:26:14 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import Story from "../components/Story.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>News Feed</h1>
+      <Story />
+    </div>
+  );
+}
